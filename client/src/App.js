5b1c8e08639434911f7b4c3c27758d1fa8b1dd1e
Seth Nelson <seth.nelson@outlook.com>
2020-03-22 18:35:11 -0700

    added useDarkMode and useLocalStorage hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import PlayerList from './components/PlayerList';
 import './App.css';
 
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players : []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(response => response.json())
+      .then(data => this.setState({ ...this.state, players : data}))
+      console.log('component mounted');
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Women's Cup Players</h1>
+        <NavBar />
+        <PlayerList players={this.state.players} />
+      </div>
+    );
+  }
+}
 
 
 export default App;
