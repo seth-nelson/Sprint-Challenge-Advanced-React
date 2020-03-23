c0339572dfabc98faf1e73e41f5e9ec3da519e67
Seth Nelson <seth.nelson@outlook.com>
2020-03-22 20:28:54 -0700

    set up a few basic tests. will try to add more meaningful ones soon

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('toggle button exists', () => {
+  const expected = render(<App/>);
+  expected.getByText(/Toggle Dark Mode/i)
+});
+
+test('players are rendered, API data returning', () => {
+  const expected = render(<App/>);
+  expected.getByText(/players/i)
 });
